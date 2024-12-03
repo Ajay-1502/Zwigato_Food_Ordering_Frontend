b2ea8c7b1767bfb03e546df8d52433c05bc0d297
Ajay-1502 <ahdesai40@gmail.com>
2024-12-03 21:58:20 +0530

    Feat : add filter to restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
 import resList from '../utils/mockData.js';
 import RestaurantCard from './RestaurantCard.js';
+import { useState } from 'react';
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="toprated-res-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.rating.aggregate_rating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((resObj) => (
+        {listOfRestaurants.map((resObj) => (
           <RestaurantCard key={resObj.info.resId} resData={resObj} />
         ))}
       </div>
