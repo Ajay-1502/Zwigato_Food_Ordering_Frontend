46f810146e86bd4b1db6378b07afdb1b80538588
Ajay-1502 <ahdesai40@gmail.com>
2025-01-01 20:50:03 +0530

    Style : Add images URL

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,5 @@
 export const LOGO_URL =
   'https://img.freepik.com/premium-vector/beautiful-unique-food-restaurant-company-logo-design_981150-2156.jpg';
+
+export const Foodimg_URL =
+  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/';
