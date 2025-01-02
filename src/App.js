86c9aef7825fdeb41520b3b368ca1bc230fa2e59
Ajay-1502 <ahdesai40@gmail.com>
2025-01-02 23:54:09 +0530

    Feat : Changed context i.e user name dynamically

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
@@ -7,17 +7,32 @@ import About from './components/About.js';
 import Contact from './components/Contact.js';
 import Error from './components/Error.js';
 import Shimmer from './components/ShimmerUI.js';
+import UserContext from './utils/UserContext.js';
 
 //import RestaurantMenu from './components/RestaurantMenu.js';
 
 const RestaurantMenu = lazy(() => import('./components/RestaurantMenu.js'));
 
 const AppLayout = () => {
+  const [userName, setUserName] = useState();
+  //Authentication Logic
+
+  useEffect(() => {
+    //Make an api call and send username and password that user has entered and in return get the name of the user
+    const data = {
+      name: 'Ajay Desai',
+    };
+
+    setUserName(data.name);
+  }, []);
+
   return (
-    <div className="app">
-      <Header />
-      <Outlet />
-    </div>
+    <UserContext.Provider value={{ loggedInUser: userName }}>
+      <div className="app">
+        <Header />
+        <Outlet />
+      </div>
+    </UserContext.Provider>
   );
 };
 
