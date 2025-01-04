7d0043fe8f1786a89b7ed2220d3674d4336b233c
Ajay-1502 <ahdesai40@gmail.com>
2025-01-04 23:42:39 +0530

    Feat : Connect redux store to application

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Contact from './components/Contact.js';
 import Error from './components/Error.js';
 import Shimmer from './components/ShimmerUI.js';
 import UserContext from './utils/UserContext.js';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore.js';
 
 //import RestaurantMenu from './components/RestaurantMenu.js';
 
@@ -27,12 +29,14 @@ const AppLayout = () => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ loggedInUser: userName }}>
-      <div className="app">
-        <Header />
-        <Outlet />
-      </div>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName }}>
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
 
